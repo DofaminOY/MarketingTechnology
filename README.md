9c7fbe8cc77328897c176f968d51afc49d6702b8
Yura O <yurkasoft@gmail.com>
2021-04-13 15:16:37 +0300

    start

---
+++ README.md
@@ -0,0 +1,19 @@
+1. The cost of a weekly subscription to the app is $7.
+
+3% of app installers are expected to choose this subscription option.
+
+The average "life expectancy" of a paid user is 10 weeks (after that the user doesn't renew the subscription anymore).
+
+The coefficient of free (organic) installations is 10% of the total number of installations.
+
+Average cost of one installation (CPI) = 1.1 $
+
+Refund rate (unsubscription, refund) = 5%
+
+How many installs does it take to get $ 10,000 in net profit (including 30% App Store commission and paid traffic costs)?
+
+How much money do we need to spend to achieve this goal?
+
+In case of a wrong answer we take into account your correct steps towards the solution.
+
+The advantage will be given to candidates who will solve this problem for any other input data.
---
+++ js/scrip.js
@@ -0,0 +1,77 @@
+window.addEventListener('DOMContentLoaded', () => {
+       // Calculator
+
+       const result = document.querySelector('.calculating__result span');
+       let calc = quantity = 20000 , commission = 30, costWeek = 7, quantitySubscriptions = 3, middleWeek = 10, noPay = 10, cpi = 1.1, returnPay = 5;
+       
+       
+       function calcTotal() {
+        
+           if (!quantity  || !commission || !costWeek || !quantitySubscriptions || !middleWeek || !noPay || !cpi || !returnPay) {
+               result.textContent = '____'; // Можете придумать что угодно
+               return;
+           } else {
+               
+               result.textContent = quantity / ((((costWeek * middleWeek) * quantitySubscriptions) / 100) - (((100 - noPay) * cpi) / 100) - ((((costWeek * middleWeek) * quantitySubscriptions) * (returnPay / 100)) / 100) - ((((costWeek * middleWeek) * quantitySubscriptions) * (commission / 100)) / 100)) 
+              
+            //   (((costWeek * middleWeek) * quantitySubscriptions) / 100)
+            //   (((100 - noPay) * cpi) / 100)
+            //   ((((costWeek * middleWeek) * quantitySubscriptions) * (returnPay / 100)) / 100)
+            //   ((((costWeek * middleWeek) * quantitySubscriptions) * (commission / 100)) / 100)
+            //   quantity / ()  
+
+           }
+       }
+   
+       calcTotal();
+   
+    
+   
+       
+       
+   
+       function getDynamicInformation(selector) {
+           const input = document.querySelector(selector);
+   
+           input.addEventListener('input', () => {
+               switch(input.getAttribute('id')) {
+                   case "quantity":
+                    quantity = +input.value;
+                       break;
+                   case "commission":
+                    commission = +input.value;
+                       break;
+                   case "costWeek":
+                    costWeek = +input.value;
+                       break;
+                   case "quantitySubscriptions":
+                    quantitySubscriptions = +input.value;
+                        break;
+                   case "middleWeek":
+                    middleWeek = +input.value;
+                        break;
+                   case "noPay":
+                    noPay = +input.value;
+                        break;
+                   case "cpi":
+                    cpi = +input.value;
+                        break;
+                   case "returnPay":
+                    returnPay = +input.value;
+                        break;
+               }
+   
+               calcTotal();
+           });
+       }
+   
+       getDynamicInformation('#quantity');
+       getDynamicInformation('#commission');
+       getDynamicInformation('#costWeek');
+       getDynamicInformation('#quantitySubscriptions');
+       getDynamicInformation('#middleWeek');
+       getDynamicInformation('#noPay');
+       getDynamicInformation('#cpi');
+       getDynamicInformation('#returnPay');
+   
+});
